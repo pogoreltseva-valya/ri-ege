e776de11057c3ce104d23e983ebd15e20bb26aca
pogoreltseva-valya <pogoreltseva45@gmail.com>
2022-10-13 23:00:28 +0300

    Intensive

---
+++ intensive/7.py
@@ -0,0 +1,24 @@
+print('Введите a и b')
+a=input()
+b=input()
+h=15*(int(a)+int(b))*(int(a)+int(b))*(int(a)+int(b))
+n=142*int(b)*int(b)
+m=222*int(a)*int(a)*int(a)*int(a)
+g=13*int(a)*int(b)
+j=int(h)-int(n)+int(m)
+t=int(j)/int(g)
+print('Значение функции равно ',t)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
