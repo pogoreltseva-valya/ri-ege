605c1a2474a8d8aa3d1975ab4be54b8911a9818e
pogoreltseva-valya <pogoreltseva45@gmail.com>
2022-10-18 21:25:43 +0300

    Intensive

---
+++ intensive/10.py
@@ -0,0 +1,20 @@
+print('Enter seat number')
+n = int(input())
+if 1<=n<=4:
+    print(1)
+elif 5<=n<=8:
+    print(2)
+if 9<=n<=12:
+    print(3)
+elif 13<=n<=16:
+    print(4)
+if 17<=n<=20:
+    print(5)
+elif 21<=n<=24:
+    print(6)
+if 25<=n<=28:
+    print(7)
+if 29<=n<=32:
+    print(8)
+elif 33<=n<=36:
+    print(9)
