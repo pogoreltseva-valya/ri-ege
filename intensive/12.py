0159b9d7dd1d884b831716f1edbedf6be2257b4a
pogoreltseva-valya <pogoreltseva45@gmail.com>
2022-10-20 21:26:35 +0300

    Intensive

---
+++ intensive/12.py
@@ -0,0 +1,9 @@
+print('Enter cell coordinates (4 numbers from 1 to 8)')
+st1 = int(input())
+str1 = int(input())
+st2 = int(input())
+str2 = int(input())
+if st1 == st2 or str1 == str2:
+    print('Yes')
+else:
+    print('No')
